3b9fbfbb0515d5245dbae6e7c9ff17be4196881b
sachin2k01 <thenamesachin@gmail.com>
2023-11-15 20:00:03 +0530

    Subtraction of two number

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -5,5 +5,15 @@ read a
 echo "enter the 2nd number"
 read b
 
+echo "enter the choice"
+echo "1: Addition"
+echo "2: Subtraction"
+echo "3: Multiplication"
+echo "4: Division"
+echo "5: Modulus"
+
 sum=$(($a+$b))
 echo sum of two number is $sum
+
+sub=$(($a-$b))
+echo sub of two number is $sub
